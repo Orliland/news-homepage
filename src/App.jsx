bc395b850dcdb61e9057657280300fb02c8833eb
Orlando Verdin <git@orli.land>
2024-03-23 22:36:33 -0700

    Add latest news components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import "./App.css";
-
-function LatestNew() {
-  return (
-    <article className="latest-new">
-      <h3 className="latest-new__title">Hydrogen VS Electric Cars</h3>
-      <p className="latest-new__body">
-        Will hydrogen-fueled cars ever catch up to EVs?
-      </p>
-    </article>
-  );
-}
+import LatestNews from "./components/LatestNews";
 
 function App() {
   return (
     <>
-      <section className="latest">
-        <h2 className="latest__title">New</h2>
-        <LatestNew />
-        <hr className="latest__split" />
-        <LatestNew />
-        <hr className="latest__split" />
-        <LatestNew />
-      </section>
+      <LatestNews />
     </>
   );
 }
