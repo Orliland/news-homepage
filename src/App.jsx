0f7d3130ce2affdcdb301104817272fac84af68a
Orlando Verdin <git@orli.land>
2024-03-23 22:11:38 -0700

    add basic latests new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import "./App.css";
+
+function LatestNew() {
+  return (
+    <article className="latest-new">
+      <h3 className="latest-new__title">Hydrogen VS Electric Cars</h3>
+      <p className="latest-new__body">
+        Will hydrogen-fueled cars ever catch up to EVs?
+      </p>
+    </article>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1>News homepage</h1>
+      <section className="latest">
+        <h2 className="latest__title">New</h2>
+        <LatestNew />
+        <hr className="latest__split" />
+        <LatestNew />
+        <hr className="latest__split" />
+        <LatestNew />
+      </section>
     </>
   );
 }
