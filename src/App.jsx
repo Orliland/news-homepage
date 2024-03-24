c3ce09d5619b3513256c4995304b20ec4e3b4060
Orlando Verdin <git@orli.land>
2024-03-24 08:07:07 -0700

    add cover component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ import News from "./components/News";
 function App() {
   return (
     <>
+      <article className="cover">
+        <img
+          className="cover__thumbnail"
+          src="../images/image-web-3-mobile.jpg"
+          alt=""
+        />
+        <div className="cover__body">
+          <h1 className="cover__title">The Bright Future of Web 3.0?</h1>
+          <p className="cover__description">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className="button">READ MORE</button>
+        </div>
+      </article>
       <LatestNews />
       <News />
     </>
